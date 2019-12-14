83a72b2d6bf6f3a9bab2ba419c6db769583fb346
vuld <vlev322@gmail.com>
2019-12-14 17:12:30 +0200

    Function Fibonacci with visualisation (js/task-1)

--- js/task-1/main.js
+++ js/task-1/main.js
@@ -8,14 +8,52 @@ function* fibonacci() {
 			const sum = seq[seq.length - 1] + seq[seq.length - 2];
 			seq.push(sum);
 		}
-		seq = seq.slice(seq.length - qnt);
+		seq = qnt < 2 ? seq.slice(seq.length - 2) : seq.slice(seq.length - qnt);
 	}
 }
 
-const n1 = fibonacci();
+const isAN = (value) => {
+	if (value instanceof Number)
+		value = value.valueOf();
 
-console.log(...n1.next().value);  // 0 1
-console.log(...n1.next(7).value); // 1 2 3 5 8 13 21
-console.log(...n1.next(7).value); // 34 55 89 144 233 377 610
-console.log(...n1.next(3).value); // 987 1597 2584
-console.log(...n1.next(8).value); // 4181 6765 10946 17711 28657 46368 75025 121393
+	return isFinite(value) && value === parseInt(value, 10);
+}
+
+const seqFib = fibonacci();
+
+seqFib.next().value;
+
+const root = document.getElementsByTagName("main")[0],
+	input = document.createElement("input"),
+	btn = document.createElement("button"),
+	wrapper = document.createElement("div"),
+	h2 = document.createElement("h2"),
+	err = document.createElement("h3"),
+	textBtn = document.createTextNode("OK"),
+	textErr = document.createTextNode("Please enter number more than 0 and number must be INTEGER!"),
+	textTitle = document.createTextNode("Enter number more than 0 and push `OK`");
+
+input.setAttribute("placeholder", "Enter integer");
+btn.appendChild(textBtn);
+h2.appendChild(textTitle);
+err.appendChild(textErr);
+
+wrapper.innerHTML += h2.outerHTML + input.outerHTML;
+root.appendChild(wrapper);
+root.appendChild(btn);
+
+const drawFib = () => {
+	const number = +document.getElementsByTagName("input")[0].value;
+	if (!isAN(number)) {
+		root.appendChild(err);
+	} else {
+		const collectionErr = document.getElementsByTagName("h3");
+		if (collectionErr.length !== 0) root.removeChild(err);
+		let sequence = seqFib.next(number).value,
+			p = document.createElement("p");
+		p.innerHTML = sequence;
+		root.appendChild(p)
+	}
+}
+
+btn.addEventListener("click", drawFib);
